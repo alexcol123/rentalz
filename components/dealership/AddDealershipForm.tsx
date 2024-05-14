a1c2ca38ed1103d69ca385a3b67ae2ef79297149
Henry Munoz <alexcol123@yahoo.com>
2024-05-14 10:41:05 -0400

    added on addDealershipForm  i added zod schema for dealership

--- components/dealership/AddDealershipForm.tsx
+++ components/dealership/AddDealershipForm.tsx
@@ -1,6 +1,11 @@
-'use client'
+"use client"
+
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
 
 import { Car, Dealership } from "@prisma/client"
+
 
 type AddDealershipProps = {
   dealership: DealershipWithCars | null
@@ -10,7 +15,63 @@ export type DealershipWithCars = Dealership & {
   cars: Car[]
 }
 
+
+const formSchema = z.object({
+
+  title: z.string().min(3, {
+    message: 'title must be atlest 2 charactes long'
+  }),
+  description: z.string().min(3, {
+    message: 'description must be atlest 2 charactes long'
+  }),
+  image: z.string().min(1, {
+    message: 'image is required'
+  }),
+  country: z.string().min(1, {
+    message: 'country is required'
+  }),
+  state: z.string().optional(),
+  city: z.string().optional(),
+  locationDescription: z.string().min(3, {
+    message: 'locationDescription must be atlest 2 charactes long'
+  }),
+  trucks: z.boolean().optional(),
+  motorcycles: z.boolean().optional(),
+  usedCars: z.boolean().optional(),
+  newCars: z.boolean().optional(),
+  electricCars: z.boolean().optional(),
+})
+
+
 const AddDealershipForm = ({ dealership }: AddDealershipProps) => {
+
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: '',
+      description: '',
+      image: '',
+      country: '',
+      state: '',
+      city: '',
+      locationDescription: '',
+      trucks: false,
+      motorcycles: false,
+      usedCars: false,
+      newCars: false,
+      electricCars: false,
+    },
+  })
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
   return (
     <div className="">AddDealershipForm</div>
   )
